9020ce6ba48d6166758ac14561938fe3734d5274
Nitish Krishna <nitishkrishna.bv@gmail.com>
2020-11-11 11:24:00 +0530

    initial xcode project creation

---
+++ Youtube-OneDayBuild/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Youtube-OneDayBuild
+//
+//  Created by Nitish Krishna on Nov/11/2020.
+//  Copyright © 2020 Technixo. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
